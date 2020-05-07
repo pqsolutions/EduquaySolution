cee61b20934fdb1f30ccedc141434cc3b8443203
mohanaveluk <mohanaveluk@gmail.com>
2020-05-06 19:22:51 -0500

    adding web presentation project files

---
+++ EduquayWeb/ClientApp/src/app/anm-module/sample-collection/sample-collection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-collection',
+  templateUrl: './sample-collection.component.html',
+  styleUrls: ['./sample-collection.component.css']
+})
+export class SampleCollectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
